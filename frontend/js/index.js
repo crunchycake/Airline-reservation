6f02a35391502a62dc6e275591d69792330d62ce
Mateusz Cieślik <mateuszcieslik@gmail.com>
2022-03-15 19:35:15 +0100

    added math.random price

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,13 @@
-var today = new Date().toISOString().split('T')[0];
-document.getElementsByName("setTodaysDate")[0].setAttribute('min', today);
+// BLOKADA DATY
+var today = new Date().toISOString().split('T')[0]
+document.getElementsByName('setTodaysDate')[0].setAttribute('min', today)
 
+// CENA LOTU
+const totalPrice = document.querySelector('.price')
+const price = ['Total Price 769 $', 'Total Price 359 $', 'Total Price 489 $', 'Total Price 299 $']
+const showPrice = () => {
+	const number = Math.floor(Math.random() * price.length)
+	totalPrice.textContent = price[number]
+}
+
+showPrice()
